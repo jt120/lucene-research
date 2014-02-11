a00675de10237df511da57390db0b52880610ef8
jt120 <jt120lz@gmail.com>
2014-02-12 00:27:21 +0800

    solr

---
+++ src/main/java/com/jt/research/lucene/solr/SolrUtil.java
@@ -0,0 +1,15 @@
+package com.jt.research.lucene.solr;
+
+
+
+public class SolrUtil {
+
+	private static String url = "http://localhost:8983/solr/";
+	private CommonsHttpSolrServer server = null;
+	public void init() {
+		
+	}
+
+
+
+}
--- src/main/java/com/jt/research/lucene/tika/TestTikaIndex.java
+++ src/main/java/com/jt/research/lucene/tika/TestTikaIndex.java
@@ -16,39 +16,41 @@ public class TestTikaIndex {
 	private SearchUtil searchUtil = null;
 	private TikaIndex tikaIndex = null;
 	private static String indexPath = "d:/test/lucene/tika/index";
+	private static String filePath = "e:\\develop\\lucene\\lucene-4.6.1\\docs\\index.html";
 	@Before
 	public void init() {
 		searchUtil = new SearchUtil(indexPath);
 		tikaIndex = new TikaIndex();
 	}
-	
+
 	@Test
 	public void index() {
-		tikaIndex.index("D:\\share\\develop\\lucene\\lucene-4.6.1\\docs");
+		tikaIndex.index(filePath);
 	}
-	
+
 	@Test
 	public void reIndex() {
 		tikaIndex.deleteAll();
-		tikaIndex.index("D:\\share\\develop\\lucene\\lucene-4.6.1\\docs");
+		tikaIndex.index(filePath);
 	}
-	
+
 	@Test
 	public void query() {
 		tikaIndex.query();
 	}
-	
+
 	@Test
 	public void testSearch() {
-		searchUtil.termQuery("fileContent", "Directory");
+		searchUtil.termQueryFile("fileContent", "High-level");
+		searchUtil.termQueryFile("fileName", "ChineseAnalyzer.html");
 	}
-	
+
 	@Test
 	public void testTika() {
-		String s = tikaIndex.fileToTxt(new File("D:/share/develop/lucene/lucene-4.6.1/docs/analyzers-common/org/apache/lucene/analysis/cjk/package-tree.html"));
+		String s = tikaIndex.fileToTxt(new File("e:/develop/lucene/lucene-4.6.1/docs/analyzers-common/org/apache/lucene/analysis/cjk/package-tree.html"));
 		System.out.println(s);
 	}
-	
+
 	@Test
 	public void testMatchAll() {
 		searchUtil.matchAllFile();
